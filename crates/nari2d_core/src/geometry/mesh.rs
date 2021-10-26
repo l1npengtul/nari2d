6acf7e565254e24a36a66735041b1c3b3ce8f925
l1npengtul <l1npengtul@protonmail.com>
2021-10-26 16:11:33 +0900

    change data structures

--- crates/nari2d_core/src/geometry/mesh.rs
+++ crates/nari2d_core/src/geometry/mesh.rs
@@ -13,9 +13,6 @@ use rstar::RTree;
 use std::cmp::Ordering;
 use std::collections::HashMap;
 
-pub type Edge = (Point2d, Point2d);
-pub type EdgeIdx = (usize, usize);
-
 // temporary triangle structs
 #[derive(Clone, Default, PartialOrd, PartialEq)]
 struct TriangleList {
@@ -37,46 +34,13 @@ impl TriangleList {
 }
 
 #[derive(Copy, Clone, PartialEq)]
-struct Triangle {
-    pub(crate) point_idx: [Point2d; 3],
-    pub(crate) adj_tri_idx: [Option<usize>; 3],
+pub struct Triangle {
+    pub(crate) point_idx: [NodeIndex; 3],
 }
 
-impl Triangle {
-    pub(crate) fn sort_ccw(&mut self) {
-        // CCW is the left most point, then the highest point that isnt the first point
-        self.point_idx.sort();
-        // find second highest point
-        let height_y_idx: usize = match float_cmp(&self.point_idx[1].y(), &self.point_idx[2].y()) {
-            Ordering::Less => 2,
-            Ordering::Equal => {
-                // in case its equal its the rightmost point
-                match float_cmp(&self.point_idx[1].x(), &self.point_idx[2].x()) {
-                    Ordering::Less => 2,
-                    Ordering::Equal => {
-                        // this shouldn't happen at all!
-                        dbg!("POINT EQUAL - WHAT???");
-                        1
-                    }
-                    Ordering::Greater => 1,
-                }
-            }
-            Ordering::Greater => 1,
-        };
+impl Triangle {}
 
-        if height_y_idx == 2 {
-            self.point_idx.swap(1, 2);
-        }
-    }
-
-    // CW is CCW but (a, b, c) => (a, c ,b)
-    pub(crate) fn sort_cw(&mut self) {
-        self.sort_ccw();
-        self.point_idx.swap(1, 2);
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Default, Hash, Ord, PartialOrd, Eq, PartialEq)]
 struct RawTriangle {
     pub(crate) points: [Point2d; 3],
 }
@@ -91,83 +55,28 @@ impl RawTriangle {
     }
 }
 
-#[derive(Copy, Clone, Debug, Hash, Ord, PartialOrd, Eq, PartialEq)]
-enum TriEdgeIdx {
-    First = 0,
-    Second = 1,
-    Third = 2,
-}
-
-struct TriangleEdge {
-    pub(crate) triangle_idx: usize,
-    pub(crate) local_tri_edge: TriEdgeIdx,
-    pub(crate) edges_point_idx: [usize; 2],
-}
-
-struct Bins {
-    bins: Vec<Vec<Point2d>>,
-    n_bins: usize,
-    grid_size: Point2d,
-    bin_size: Point2d,
-}
-
-impl Bins {
-    pub fn new(n_bins: usize, size: Scale2d) -> Self {
-        Bins {
-            bins: vec![vec![]; n_bins],
-            n_bins,
-            grid_size: size.into(),
-            bin_size: (size / (n_bins as f32)).into(),
-        }
-    }
-
-    pub fn push(&mut self, point: Point2d) {
-        let i = ((0.99_f32 * (self.n_bins as f32) * point.y()) / self.grid_size.y()) as usize;
-        let j = ((0.99_f32 * (self.n_bins as f32) * point.x()) / self.grid_size.x()) as usize;
-
-        let mut b = if i % 2 == 0 {
-            i * self.n_bins + j + 1
-        } else {
-            (i + self.n_bins) * self.n_bins - j
-        };
-
-        // normalize to zero
-        b -= 1;
-
-        self.bins[b].push(point);
-    }
-
-    pub fn get(&self, index: usize) -> Option<&Point2d> {
-        self.bins.iter().flatten().nth(index)
-    }
-
-    pub fn get_bin(&self, bin: usize, index: usize) -> Option<&Point2d> {
-        self.bins[bin].get(index)
-    }
-
-    pub fn to_flattened(self) -> Vec<Point2d> {
-        self.bins.into_iter().flatten().collect::<Vec<Point2d>>()
-    }
-}
-
 pub struct Mesh {
     mesh: StableGraph<Point2d, ()>,
-    input_points: HashMap<Point2d, NodeIndex, RandomState>,
+    mesh_mod: StableGraph<Point2d, ()>,
+    points_store: HashMap<Point2d, NodeIndex>,
+    triangles: Vec<Triangle>,
     // add more, communist
 }
 
 impl Mesh {
     pub fn new() -> Self {
         Mesh {
-            mesh: StableGraph::new(),
-            input_points: HashMap::default(),
+            mesh: StableGraph::default(),
+            mesh_mod: StableGraph::default(),
+            points_store: HashMap::default(),
+            triangles: vec![],
         }
     }
 
     // https://www.newcastle.edu.au/__data/assets/pdf_file/0019/22519/23_A-fast-algortithm-for-generating-constrained-Delaunay-triangulations.pdf
     // https://www.habrador.com/tutorials/math/14-constrained-delaunay/
     // https://forum.unity.com/threads/programming-tools-constrained-delaunay-triangulation.1066148/
-    pub fn new_with_points_and_constraints(
+    pub fn with_points_and_constraints(
         points: Vec<Point2d>,
         constraints: Vec<EdgeIdx>,
     ) -> NResult<Self> {
@@ -179,45 +88,31 @@ impl Mesh {
         }
 
         let mut points = points;
-        let constraints = {
-            let mut edges = Vec::with_capacity(constraints.len());
-            for edge_index in constraints {
-                let real_edge: Edge = match (points.get(edge_index.0), points.get(edge_index.1)) {
-                    (Some(v), Some(b)) => (*v, *b),
-                    (_, _) => {
-                        return Err(Nari2DError::InvalidMesh {
-                            points,
-                            error: "Invalid Edge Constraints".to_string(),
-                        })
-                    }
-                };
-                edges.push(real_edge)
-            }
-            edges
-        };
 
-        // create the graph & tree
-        let mut mesh: StableGraph<Point2d, (), Undirected, DefaultIx> =
-            StableGraph::with_capacity(points.len(), points.len() * 3);
-        let mut input_points: HashMap<Point2d, NodeIndex, RandomState> =
-            HashMap::with_capacity_and_hasher(points.len(), RandomState::new());
+        // create the meshes
+        let mut mesh = StableGraph::with_capacity(
+            points.len() + 3,
+            (points.len() as f32 * 1.5_f32).round() as usize + 3,
+        );
+        let mut points_store = HashMap::with_capacity(points.len() + 3);
+        let triangles = Vec::with_capacity((points.len() as f32 / 3_f32) as usize + 1);
 
         let supertriangle = calculate_super_triangle(&points);
         supertriangle.to_vec().for_each(|(pt_from, pt_to)| {
-            let from_id = match input_points.get(&pt_from) {
-                Some(idx) => *idx,
+            let from_id = match points_store.get(&pt_from) {
+                Some(idx) => idx.clone(),
                 None => {
                     let id = mesh.add_node(pt_from);
-                    input_points.insert(pt_from, id);
+                    points_store.insert(pt_from, id.clone());
                     id
                 }
             };
 
-            let to_id = match input_points.get(&pt_to) {
-                Some(idx) => *idx,
+            let to_id = match points_store.get(&pt_to) {
+                Some(idx) => idx.clone(),
                 None => {
                     let id = mesh.add_node(pt_to);
-                    input_points.insert(pt_to, id);
+                    points_store.insert(pt_to, id.clone());
                     id
                 }
             };
