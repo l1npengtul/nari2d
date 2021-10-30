880e79993b709a211459f42e2091c09356d00cd7
l1npengtul <l1npengtul@protonmail.com>
2021-10-30 13:34:55 +0900

    finish new

--- crates/nari2d_core/src/geometry/mesh.rs
+++ crates/nari2d_core/src/geometry/mesh.rs
@@ -1,28 +1,56 @@
-use crate::error::{Nari2DError, NResult};
-use crate::geometry::Point2d;
+use crate::{
+    error::{NResult, Nari2DError},
+    geometry::Point2d,
+};
 use ahash::RandomState;
 use std::collections::HashMap;
 
 pub type Triangle = (usize, usize, usize);
+pub type Edge = (usize, usize);
 
+#[derive(Clone, Default, Debug, PartialEq)]
 pub struct Mesh {
-    points: Vec<Point2d>,
     points_mod: Vec<Point2d>,
-    constraints: Vec<usize>,
-    triangles: Vec<Triangle>,
+    constraints: Vec<Edge>,
+    triangulation: Vec<Triangle>,
     lookup_table: HashMap<Point2d, usize, RandomState>,
 }
 
 impl Mesh {
-    pub fn new(points: Vec<Point2d>, constraints: Vec<usize>) -> NResult<Self> {
-        // load the points into the hashmap
-        let mut lookup_table = HashMap::with_capacity(points.len());
-        for (index, point) in points.iter().enumerate() {
-            if let Some(_) = lookup_table.insert(*point, index) {
-                return Err(Nari2DError::InvalidMesh { points, error: format!("Point {} added again!", point) })
+    /// # Errors
+    pub fn new(input_points: Vec<Point2d>, constraints: Vec<Edge>) -> NResult<Self> {
+        let pts = input_points
+            .as_slice()
+            .iter()
+            .map(|pt| {
+                let (x, y) = (f64::from(pt.x()), f64::from(pt.y()));
+                (x, y)
+            })
+            .collect::<Vec<(f64, f64)>>();
+
+        let triangulation = match cdt::triangulate_with_edges(&pts, constraints.as_slice()) {
+            Ok(tri) => tri as Vec<Triangle>,
+            Err(why) => {
+                return Err(Nari2DError::InvalidMesh {
+                    points: input_points,
+                    error: why.to_string(),
+                })
             }
+        };
+
+        let mut lookup_table =
+            HashMap::with_capacity_and_hasher(input_points.len(), RandomState::new());
+        for triangle in &triangulation {
+            lookup_table.insert(input_points[triangle.0], triangle.0);
+            lookup_table.insert(input_points[triangle.1], triangle.1);
+            lookup_table.insert(input_points[triangle.2], triangle.2);
         }
 
-        let triangulation = cdt::triangulate_with_edges()
+        Ok(Mesh {
+            points_mod: lookup_table.keys().into_iter().copied().collect(),
+            constraints,
+            triangulation,
+            lookup_table,
+        })
     }
 }
